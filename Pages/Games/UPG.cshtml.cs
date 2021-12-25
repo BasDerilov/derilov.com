223741e548779a4ea78c106d0fccddf89a68dbf5
Vasil <vasil.derilov@gmail.com>
2021-12-25 12:04:47 +0200

    added pixi JS and started work on next game

---
+++ Pages/Games/UPG.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using fictionsplash.Pages;
+
+namespace fictionsplash.Pages.Games
+{
+    public class UPGModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
